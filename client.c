043afe87e4399203cb0e6362550a3b5fdebd9346
Michał Kowalski <60048782+XxYeetusMinecraftxX@users.noreply.github.com>
2024-10-11 11:23:13 +0200

    Update client.c

--- client.c
+++ client.c
@@ -4,31 +4,40 @@
 #include <winsock2.h>
 #include <unistd.h>
 
-typedef struct {
-    int var;
-} data;
+typedef struct{
+    int x,y,z;
+} pdata;
 
 int main(int argc, char *argv[]){
 
     WSADATA wsa;
     SOCKET sck;
     char addr[] = "";
-    char buf[1024] = "dupa";
-    int port = 8000;
+    char buf[1024];
+    int port = 8080;
     struct sockaddr_in server;
-    data pck;
+
+    pdata pck;
+
+    pck.x = 5;
+    pck.y = 10;
+    pck.z = 15;
 
     printf("Input address:\n");
     scanf("%s", addr);
     printf("Input port:\n");
     scanf("%d", &port);
-    printf("Input var:\n");
-    scanf("%d", &pck.var);
 
     WSAStartup(MAKEWORD(2,2), &wsa) != 0;
 
     if( (sck = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ){
         printf("s", "broken socket");
+    }
+
+    if(addr == "localhost"){
+        server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    } else {
+        server.sin_addr.s_addr = inet_addr(addr);
     }
 
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -39,10 +48,16 @@ int main(int argc, char *argv[]){
     int n;
 
     while(1){
-        printf("x\n");
+
+        printf("Input data x y z:\n");
+        scanf("%d", &pck.x);
+        scanf("%d", &pck.y);
+        scanf("%d", &pck.z);
+
+        memcpy(&buf, &pck, sizeof(pdata));
+
         n = sendto(sck, buf, sizeof(buf), 0, (const struct sockaddr *) & server, sizeof(server));
-        printf("%d\n", WSAGetLastError());
-        sleep(3);
+        //printf("%d\n", WSAGetLastError());
     }
 
     close(sck);
